84889b79c853286daf9d4d273090e05bcaba47fb
Kamil Mołodecki <109474440+KamilM13@users.noreply.github.com>
2025-04-09 13:58:02 +0200

    weird

--- geoapps/geoapps/lab03/cloud_mosaic.py
+++ geoapps/geoapps/lab03/cloud_mosaic.py
@@ -3,9 +3,11 @@ import ee
 ee.Initialize(project='ee-kamilmolo13')
 
 def create_cloud_free_mosaic(aoi, start_date, end_date):
-    image_collection = ee.ImageCollection('COPERNICUS/S2_HARMONIZED') \
+    image_collection = ee.ImageCollection('COPERNICUS/S2') \
     .filterDate(start_date, end_date) \
     .filterBounds(aoi) \
+    .sort('CLOUDY_PIXEL_PERCENTAGE') \
+    .first()
 
     def cloud_mask(image):
         qa_band = image.select('QA60')
@@ -14,6 +16,8 @@ def create_cloud_free_mosaic(aoi, start_date, end_date):
         c_mask = qa_band.bitwiseAnd(cloud_bit).eq(0).And(qa_band.bitwiseAnd(cirrus_bit).eq(0))
         return image.updateMask(c_mask).copyProperties(image, image.propertyNames())
     
-    mosaic = image_collection.map(cloud_mask).median().clip(aoi)
+    #m_c = image_collection.map(cloud_mask)
+    #mosaic = m_c.median().clip(aoi)
+    mosaic = image_collection
 
     return mosaic
